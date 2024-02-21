4876c704aaa2c91fdfec72eafb5db73f9637375d
harshita795 <yadavharshita261303@gmail.com>
2024-02-21 16:09:40 +0530

    feat: upload modal and functionality

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,11 +1,25 @@
 "use client";
 
+import useAuthModal from "@/hooks/useAuthModal";
+import useUploadModal from "@/hooks/useUploadModal";
+import { useUser } from "@/hooks/useUser";
+
 import { AiOutlinePlus } from "react-icons/ai";
 import { TbPlaylist } from "react-icons/tb";
 
 const Library = () => {
+  const authModal = useAuthModal();
+  const uploadModal = useUploadModal();
+  const { user } = useUser();
   const onClick = () => {
-    // Handle upload Later
+    if (!user) {
+      return authModal.onOpen();
+    }
+
+    //  todo : check for subscription
+
+    return uploadModal.onOpen();
+
   };
   return (
     <div className="flex flex-col">
--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import AuthModal from "@/components/AuthModal";
+import UploadModal from "@/components/UploadModal";
 
 
 import { useEffect, useState } from "react";
@@ -19,6 +20,7 @@ const ModalProvider = () => {
   return (
     <>
       <AuthModal />
+      <UploadModal />
     </>
   );
 }
