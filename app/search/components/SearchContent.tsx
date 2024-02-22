15574accead911c12665155b73214d7a66803ae3
harshita795 <yadavharshita261303@gmail.com>
2024-02-22 13:57:18 +0530

    feat: favourites functionality

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import LikeButton from "@/components/LikeButton";
 import MediaItem from "@/components/MediaItem";
 import { Song } from "@/types";
 
@@ -37,7 +38,7 @@ const SearchContent: React.FC<SearchContentProps> = ({
               data={song}
             />
           </div>
-          {/* add like button here : todo */}
+          <LikeButton songId={song.id} />
         </div>
       ))}
     </div>
