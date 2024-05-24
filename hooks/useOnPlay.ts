675e9dd150f52a82b75ed73893dba20b9f6dd9cc
harshita795 <yadavharshita261303@gmail.com>
2024-05-24 17:05:32 +0530

    upgrade

--- hooks/useOnPlay.ts
+++ hooks/useOnPlay.ts
@@ -2,23 +2,23 @@ import { Song } from "@/types";
 import usePlayer from "./usePlayer";
 import useAuthModal from "./useAuthModal";
 import { useUser } from "./useUser";
-import useSubscribeModal from "./useSubscribeModal";
+// import useSubscribeModal from "./useSubscribeModal";
 
 
 const useOnPlay = (songs: Song[]) => {
   const player = usePlayer();
-  const subscribeModal = useSubscribeModal();
+  // const subscribeModal = useSubscribeModal();
   const authModal = useAuthModal();
   const { user, subscription } = useUser();
 
   const onPlay = (id: string) => {
-    if (!user) {
-      return authModal.onOpen();
-    }
+    // if (!user) {
+    //   return authModal.onOpen();
+    // }
 
-    if (!subscription) {
-      return subscribeModal.onOpen();
-    }
+    // if (!subscription) {
+    //   return subscribeModal.onOpen();
+    // }
 
     player.setId(id);
     player.setIds(songs.map((song) => song.id));
